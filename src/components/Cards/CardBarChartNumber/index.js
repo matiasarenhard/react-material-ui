581e494af6e303ee3ae3b3a4f7588844088e9a90
Matias <matias_arenhard@hotmail.com>
2022-05-23 09:28:45 -0300

    refactory, add data json

--- src/components/Cards/CardBarChartNumber/index.js
+++ src/components/Cards/CardBarChartNumber/index.js
@@ -2,53 +2,11 @@ import * as React from "react";
 import Card from "@mui/material/Card";
 import styles from "./CardBarChartNumber.module.css";
 import CardContent from "@mui/material/CardContent";
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
-const dataChart2 = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import data  from "../../../api/barchartnumber.json";
 
 export default function CardButton() {
+  const dataChart = data;
   return (
     <>
       <Card sx={{ maxWidth: 800 }} className={styles.body}>
@@ -56,7 +14,7 @@ export default function CardButton() {
               <BarChart
                 width={200}
                 height={200}
-                data={dataChart2}
+                data={dataChart.data}
               >
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="name" />
--- src/components/Cards/CardPie/index.js
+++ src/components/Cards/CardPie/index.js
@@ -3,26 +3,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import styles from "./CardPie.module.css";
 import { PieChart, Pie } from 'recharts';
-
-const data01 = [
-  { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 },
-];
-const data02 = [
-  { name: 'A1', value: 100 },
-  { name: 'A2', value: 300 },
-  { name: 'B1', value: 100 },
-  { name: 'B2', value: 80 },
-  { name: 'B3', value: 40 },
-  { name: 'B4', value: 30 },
-  { name: 'B5', value: 50 },
-  { name: 'C1', value: 100 },
-  { name: 'C2', value: 200 },
-  { name: 'D1', value: 150 },
-  { name: 'D2', value: 50 },
-];
+import data  from "../../../api/piechart.json";
 
 export default function CardButton() {
   return (
@@ -30,8 +11,8 @@ export default function CardButton() {
       <Card className={styles.body}>
         <CardContent >
           <PieChart width={200} height={200} margin={0}>
-            <Pie data={data01} dataKey="value" cx="50%" cy="50%" outerRadius={30} fill="#8884d8" />
-            <Pie data={data02} dataKey="value" cx="50%" cy="50%" innerRadius={30} outerRadius={50} fill="#82ca9d" label />
+            <Pie data={data.data01} dataKey="value" cx="50%" cy="50%" outerRadius={30} fill="#8884d8" />
+            <Pie data={data.data02} dataKey="value" cx="50%" cy="50%" innerRadius={30} outerRadius={50} fill="#82ca9d" label />
           </PieChart>
         </CardContent>
       </Card>
